720946c7306567de727f3bd9ab10c0af2c1c3ff9
minjiiiiii <81156796+MINZIIII717@users.noreply.github.com>
2021-11-03 17:54:22 +0900

    Update project.py

--- project.py
+++ project.py
@@ -3,6 +3,17 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder, OrdinalEncoder, OneHotEncoder
 from sklearn.preprocessing import StandardScaler, RobustScaler, MaxAbsScaler, MinMaxScaler
 
+# ----------------------------------- Functions -----------------------------------
+def nativeCountry(x) :
+  if x != "United-States" :
+    return str(x).replace(str(x),"0")
+  return "1"
+
+def income(x) :
+  if x == '<=50K' :
+    return x.replace(x,"0")
+  return "1"
+
 
 # Load the dataset (adult.csv)
 df = pd.read_csv('adult.csv')
@@ -17,4 +28,22 @@ df = df.dropna(axis=0)
 # 3. Drop columns (education, capital-gain, capital-loss)
 df.drop(['education', 'capital-gain', 'capital-loss'], axis=1, inplace=True)
 
-print(df.columns)
+# 4. Change "native-contry" values to binary value
+# "United-States" : 1, not "United-States" : 0
+df["native-country"]=df["native-country"].apply(nativeCountry)
+
+# 5. Change "income" values to binary value
+# <=50k : 2, >50k :1
+df["income"]=df["income"].apply(income)
+
+# 6. Change educational number to three sector
+# <10 : 1, 10~13 : 2, >13 :3
+df["educational-num"]=df["educational-num"].mask(df["educational-num"] < 10, 1)
+df["educational-num"]=df["educational-num"].mask(df["educational-num"] == 10, 2)
+df["educational-num"]=df["educational-num"].mask(df["educational-num"] == 11, 2)
+df["educational-num"]=df["educational-num"].mask(df["educational-num"] == 12, 2)
+df["educational-num"]=df["educational-num"].mask(df["educational-num"] == 13, 2)
+df["educational-num"]=df["educational-num"].mask(df["educational-num"] > 13, 3)
+
+pd.set_option('display.max_columns', None)
+print(df)
